004175c682c8954cec3750268df3c357c3df89ad
Jensen Judkins <thesmelliestman@gmail.com>
2023-01-27 01:10:46 -0700

    made it look alot better

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,29 @@ function App() {
 
 
   
-  
+  async function randomQuote() {
+    const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random")
+    
+      
+      //save all json results to data, multiple authors and quotes will come saved in an array under results[]
+      const data = await result.json();
+
+      var returnMe = "";
+
+      //for each results in array found in the json, push to quotes array
+      data.results.forEach((quote: Quote) => {
+        //quotes.push is not a function
+        //setQuotes(quotes => [...quotes, quote])
+        //console.log(quote)
+
+        returnMe = returnMe + "<div>" + quote.content + "<br>" + quote.author + "<br><br></div> ";
+        
+      })
+      //console.log(returnMe)
+      
+      return returnMe;
+
+  }
 
 
 
@@ -50,12 +72,13 @@ function App() {
 
   return (
           
-          <div>
+          <div id="everything">
             <h1>Random Quote</h1>
             <div>
               <input type="text"
                 value={author}
                 onChange={(e) => setAuthor(e.target.value)}/>
+                <br></br>
                 <button onClick={() => {
                   //console.log(author);
                   //fetchQuote(author).then((data) => {
@@ -73,10 +96,25 @@ function App() {
                   
 
                   }>Search</button>
+                
+                {/* when document first loads, run the randomQuote function */}
 
-                  <div id="quotes">
-                    
-                  </div>
+              <div id="startup" onLoad={() => {
+                  console.log("startup")
+                  randomQuote().then((data) => {
+                    console.log(data)
+                    document.getElementById("quotes")!.innerHTML = data;
+                  })
+                }
+              }>
+              </div>
+
+
+
+
+
+              <div id="quotes">
+              </div>
                   
                   
               
