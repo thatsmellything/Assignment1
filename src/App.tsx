68089c68b4e33afaa518d9cc89877b9899e4cbcf
Jensen Judkins <thesmelliestman@gmail.com>
2023-01-27 13:56:29 -0700

    making it prettier, fixed it reloading quotes when search bar changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,67 +18,65 @@ function App() {
 
   //this should be called when the page first loads and acts very similarly to the fetchQuote function it is just a random quote instead
   async function randomQuote() {
-    
-
     //console.log("fetch quote called")
       //this is the results of the actual fetch
       const result = await fetch("https://usu-quotes-mimic.vercel.app/api/random")
-      
       //save all json results to data, multiple authors and quotes will come saved in an array under results[]
       const data = await result.json();
-
       //create variable to hold the html to be returned
       var returnMe = "";
-
       //returns a single quote and author
-      
       const quote = data as Quote;
-
       //this takes each quote and ite respective author and adds it to returnMe, which is basic html
-      returnMe = returnMe + "<div>" + quote.content + "<br>" + quote.author + "<br><br></div> ";
-        
-    
       
+      returnMe = returnMe + "<div id='singleQuote'>" + quote.content + "<br>" + quote.author + "<br></div><br> ";
       //return the basic html
       return returnMe;
 
   }
 
-  function loadingIn() {
+
+  async function fetchQuote(searchAuthor: string) {
+    //console.log("fetch quote called")
+      //this is the results of the actual fetch
+      const result = await fetch("https://usu-quotes-mimic.vercel.app/api/search?query=" + searchAuthor)
+      //save all json results to data, multiple authors and quotes will come saved in an array under results[]
+      const data = await result.json();
+      //create variable to hold the html to be returned
+      var returnMe = "";
+      //for each results in array found in the json, push to quotes array
+      data.results.forEach((quote: Quote) => {
+        //this takes each quote and ite respective author and adds it to returnMe, which is basic html
+        returnMe = returnMe + "<div id='singleQuote'>" + quote.content + "<br>" + quote.author + "<br></div><br> ";
+      })
+      //return the basic html
+      return returnMe;
+  }
+
+  function setBgColorById(id: string,sColor: string) {
+    var elem;
+    //for each element in the document, if the id matches the id passed in, set the background color to the color passed in
+    document.querySelectorAll("#singleQuote").forEach((elem) => {
+      elem.style.backgroundColor = sColor;
+    })
+   }
+
+
+ 
+  useEffect(() => {
     //when document first loads, run the randomQuote function
     randomQuote().then((data) => {
       //put the data into the div with id quotes
       document.getElementById("quotes")!.innerHTML = data;
+      setBgColorById("singleQuote", "gray");
     })
-  }
+  }, [])
 
-  loadingIn();  
+    //loadingIn();
+  
+  
 
-
-
-  async function fetchQuote(searchAuthor: string) {
-    
-
-    //console.log("fetch quote called")
-      //this is the results of the actual fetch
-      const result = await fetch("https://usu-quotes-mimic.vercel.app/api/search?query=" + searchAuthor)
-      
-      //save all json results to data, multiple authors and quotes will come saved in an array under results[]
-      const data = await result.json();
-
-      //create variable to hold the html to be returned
-      var returnMe = "";
-
-      //for each results in array found in the json, push to quotes array
-      data.results.forEach((quote: Quote) => {
-        //this takes each quote and ite respective author and adds it to returnMe, which is basic html
-        returnMe = returnMe + "<div>" + quote.content + "<br>" + quote.author + "<br><br></div> ";
-        
-      })
-      
-      //return the basic html
-      return returnMe;
-  }
+  
 
 
 
@@ -100,40 +98,13 @@ function App() {
                   fetchQuote(author).then((data) => {
                     //put the data into the div with id quotes
                     document.getElementById("quotes")!.innerHTML = data;
+                    setBgColorById("singleQuote", "gray");
                   })
                   
                 }
-                  }>Search</button>
-
-
-
-
-
-
-
-                
-              {/* when document first loads, run the randomQuote function */}
-
-              <div id="startup">
-                  
-
-              </div>
-
-
-
-
-
-              <div id="quotes">
-              </div>
-                  
-                  
-              
+                  }>Search</button>              
+              <div id="quotes"></div>
             </div>
-
-            
-            
-            
-            
           </div>
   )
 }
