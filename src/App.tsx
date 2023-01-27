a2630a9a3b12681185c9e4bd4740530d8b62e266
Jensen Judkins <thesmelliestman@gmail.com>
2023-01-27 14:22:19 -0700

    added comments

--- src/App.tsx
+++ src/App.tsx
@@ -28,14 +28,12 @@ function App() {
       //returns a single quote and author
       const quote = data as Quote;
       //this takes each quote and ite respective author and adds it to returnMe, which is basic html
-      
       returnMe = returnMe + "<div id='singleQuote'>" + quote.content + "<br>" + quote.author + "<br></div><br> ";
       //return the basic html
       return returnMe;
-
   }
 
-
+// take in the author to search for, output a bunch of the returned quotes in the form of mini div's with the quote and author
   async function fetchQuote(searchAuthor: string) {
     //console.log("fetch quote called")
       //this is the results of the actual fetch
@@ -44,9 +42,10 @@ function App() {
       const data = await result.json();
       //create variable to hold the html to be returned
       var returnMe = "";
-      //for each results in array found in the json, push to quotes array
+      //for each results in array found in the json, push to quotes array, this is because it sends back an array of quotes 
+      //unlike the random quote which just sends one
       data.results.forEach((quote: Quote) => {
-        //this takes each quote and ite respective author and adds it to returnMe, which is basic html
+        //this takes each quote and its respective author and adds it to returnMe, which is basic html
         returnMe = returnMe + "<div id='singleQuote'>" + quote.content + "<br>" + quote.author + "<br></div><br> ";
       })
       //return the basic html
@@ -54,9 +53,7 @@ function App() {
   }
 
  
-
-
- 
+ //this basically will run when the focument first loads or if the page is refreshed
   useEffect(() => {
     //when document first loads, run the randomQuote function
     randomQuote().then((data) => {
@@ -65,13 +62,6 @@ function App() {
       
     })
   }, [])
-
-    //loadingIn();
-  
-  
-
-  
-
 
 
 
@@ -82,6 +72,8 @@ function App() {
             <div>
               {/* text input for the author search */}
               <input type="text"
+                placeholder="Author to search for"
+                // straight up copied this from our notes thing so idk if its really even needed but it works sooooo
                 value={author}
                 onChange={(e) => setAuthor(e.target.value)}/>
                 <br></br>
@@ -96,7 +88,8 @@ function App() {
                   })
                   
                 }
-                  }>Search</button>              
+                  }>Search</button>
+              {/* div to hold the quotes */}
               <div id="quotes"></div>
             </div>
           </div>
